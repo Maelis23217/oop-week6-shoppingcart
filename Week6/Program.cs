a88b32b5e99df7ea3b41de787f3a3e52138d6f5b
Maelis23217 <71397438+Maelis23217@users.noreply.github.com>
2020-10-21 17:09:20 +0200

    Add files via upload

---
+++ Week6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Week6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Shopping_Cart shop = new Shopping_Cart();
+
+            Orange o1 = new Orange();
+            Orange o2 = new Orange();
+            Orange o3 = new Orange();
+
+            shop.orange.Add(o1);
+            shop.orange.Add(o2);
+            shop.orange.Add(o3);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
